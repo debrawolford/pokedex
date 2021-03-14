9598a0ebcfc529ffbf6fa366fab9fe970ac70e06
Debra Wolford <debra.wolford@gmail.com>
2021-03-14 09:54:15 +0100

    Added home template and view

--- pokemon/views.py
+++ pokemon/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Pokedex Home</h1>')
+    template = 'pokemon/home.html'
+    return render(request, template)
