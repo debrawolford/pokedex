7deb20f79c05789eb41d5f3420d396d4a7da72d4
Debra Wolford <debra.wolford@gmail.com>
2021-03-14 13:27:51 +0100

    created models and tested API request

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Ability(models.Model):
+    class Meta:
+        verbose_name_plural = 'Abilities'
+    
+    name = models.CharField(max_length=254)
+    description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+
+class Pokemon(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+    ability = models.ForeignKey('Ability', null=True, blank=True, on_delete=models.CASCADE)
+    height = models.IntegerField()
+    sprite_url = models.URLField(max_length=1024, null=True, blank=True)
+
+def __str__(self):
+        return self.name
--- pokemon/views.py
+++ pokemon/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import requests
+
+pokemon = requests.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=150')
+
 def home(request):
     template = 'pokemon/home.html'
-    return render(request, template)
+    context = {
+        'pokemon': pokemon,
+    }
+    return render(request, template, context)
