8d9e8187bae27494cfdc9c79b6ed48937dfb4aa0
Debra Wolford <debra.wolford@gmail.com>
2021-03-15 14:37:29 +0100

    Access Pokeapi data

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,13 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import json
 import requests
 
-pokemon = requests.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=150')
+offset = 0
+response = requests.get(f'https://pokeapi.co/api/v2/pokemon/?offset={offset}&limit=20')
+response_json = response.json()
+
+for pokemon in response_json["results"]:
+    name = pokemon["name"]
+    url = pokemon["url"]
+    res = requests.get(f'{url}')
+    res_json = res.json()
+    height = res_json["height"]
+    _id = res_json["order"]
+    sprite = res_json["sprites"]["front_default"]
+    if len(res_json["abilities"]) == 1:
+        ability1 = res_json["abilities"][0]["ability"]["name"]
+        print(name, height, _id, sprite, ability1)
+    elif len(res_json["abilities"]) == 2:
+        ability1 = res_json["abilities"][0]["ability"]["name"]
+        ability2 = res_json["abilities"][1]["ability"]["name"]
+        print(name, height, _id, sprite, ability1, ability2)
+    elif len(res_json["abilities"]) == 3:
+        ability1 = res_json["abilities"][0]["ability"]["name"]
+        ability2 = res_json["abilities"][1]["ability"]["name"]
+        ability3 = res_json["abilities"][2]["ability"]["name"]
+        print(name, height, _id, sprite, ability1, ability2, ability3)
+
 
 def home(request):
     template = 'pokemon/home.html'
     context = {
-        'pokemon': pokemon,
+        'pokemon': response,
     }
     return render(request, template, context)
