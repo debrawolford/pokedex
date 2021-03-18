d20fb0f4853c3a8cccb8716c61944e7a29c0a97f
Debra Wolford <debra.wolford@gmail.com>
2021-03-18 13:49:15 +0100

    added ability buttons and bootstrap styling

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="pokemon-home"),
+    path('', views.pokemon, name="pokemon"),
 ]
--- pokemon/views.py
+++ pokemon/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from .models import Ability, Pokemon
 
 
 
-# View to display home page with all pokemon
-def home(request):
+# View to display home page with all pokemon and images
+def pokemon(request):
     template = 'pokemon/home.html'
+    pokemon = Pokemon.objects.all()
+    abilities = Ability.objects.all()
+    query = None
+    ability = None
+    if request.GET:
+        if 'ability' in request.GET:
+            ability = request.GET['ability'].split()
+            pokemon = pokemon.filter(ability__name__in=ability)
+            ability = Ability.objects.filter(name__in=ability)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, ("Please enter what you are"
+                                         " looking for."))
+                return redirect(reverse('pokemon'))
+
+            queries = (
+                Q(name__icontains=query)
+            )
+            pokemon = pokemon.filter(queries)
+
+
     context = {
-        'pokemon': Pokemon.objects.all(),
-        'abilities': Ability.objects.all()
+        'pokemon': pokemon,
+        'abilities': abilities,
+        'search': query,
+        'current_ability': ability
     }
     return render(request, template, context)
